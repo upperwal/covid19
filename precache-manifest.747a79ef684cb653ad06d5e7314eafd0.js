45e23af1af7d93ecb8872ca9d64f1e6556747bf5
Abhishek Upperwal <a.upperwal@gmail.com>
2020-03-21 18:51:31 +0530

    Updates

---
+++ precache-manifest.747a79ef684cb653ad06d5e7314eafd0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7b0a91ee6300cc4ef473fd702fe416",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "84d73570f166d13d7ef0",
+    "url": "/covid19/static/css/main.818e20dc.chunk.css"
+  },
+  {
+    "revision": "a6197e66deb16b21703f",
+    "url": "/covid19/static/js/2.077b3cfc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/covid19/static/js/2.077b3cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d73570f166d13d7ef0",
+    "url": "/covid19/static/js/main.adb804e5.chunk.js"
+  },
+  {
+    "revision": "79ff7798925f0c35a102",
+    "url": "/covid19/static/js/runtime-main.93cd156c.js"
+  },
+  {
+    "revision": "c5f954788f341b22e1974433bb972ac1",
+    "url": "/covid19/static/media/Quicksand-Light.c5f95478.ttf"
+  },
+  {
+    "revision": "0c64233241ead44bffbec54eb9d1d164",
+    "url": "/covid19/static/media/Quicksand-Medium.0c642332.ttf"
+  },
+  {
+    "revision": "f87b9b4f34bdbf75b5c0cf3a5a137508",
+    "url": "/covid19/static/media/Quicksand-Regular.f87b9b4f.ttf"
+  },
+  {
+    "revision": "8458c828e152dfacb65ec68777fd34d1",
+    "url": "/covid19/static/media/cover_face.8458c828.svg"
+  },
+  {
+    "revision": "2f3c3d90943cc480276beca4e0d9c7c3",
+    "url": "/covid19/static/media/dont_touch_face.2f3c3d90.svg"
+  },
+  {
+    "revision": "0a2b4dabf6df34aae8f356601b688fed",
+    "url": "/covid19/static/media/face_mask.0a2b4dab.svg"
+  },
+  {
+    "revision": "f0a5d838f358976204cd23181d48fe00",
+    "url": "/covid19/static/media/handrub.f0a5d838.svg"
+  },
+  {
+    "revision": "2708ccafcba19598e6cb3aa69e51321e",
+    "url": "/covid19/static/media/handwash.2708ccaf.svg"
+  },
+  {
+    "revision": "f11225ad3fa1d3d820afb4135ca95ef2",
+    "url": "/covid19/static/media/social_distancing.f11225ad.svg"
+  }
+]);
