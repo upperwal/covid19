d7c1db83047e5bc7bf7a29b6403ad2384ad5cb6e
Anurag Upperwal <anuragupperwal@gmail.com>
2020-05-21 22:43:02 +0530

    commented random function

--- src/userpages/home/Home.js
+++ src/userpages/home/Home.js
@@ -29,11 +29,11 @@ class HomeComponent extends React.Component {
         }
     }
 
-    getRandomInt(min, max){
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+    // getRandomInt(min, max){
+    //     min = Math.ceil(min);
+    //     max = Math.floor(max);
+    //     return Math.floor(Math.random() * (max - min + 1)) + min;
+    // }
 
 
     mostAffectedView(statesList, stateCodeNameMap, theme, label) {
