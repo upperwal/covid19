d1f68f34b844384a4543b4d7532210c28435e6b1
Anurag Upperwal <anuragupperwal@gmail.com>
2020-05-25 13:05:11 +0530

    corrected 'and' problem when 1 state is left

--- src/userpages/home/Home.js
+++ src/userpages/home/Home.js
@@ -70,8 +70,10 @@ class HomeComponent extends React.Component {
 
         let recoveryPathView = []
         recoveryPath.forEach((s, idx) => {
-            if(idx === recoveryPath.length - 1) {
-                recoveryPathView.push(<span key="and">{' and '}</span>)
+            if(recoveryPath.length!==1){
+                if(idx === recoveryPath.length - 1) {
+                    recoveryPathView.push(<span key="and">{' and '}</span>)
+                }
             }/*  else {
                 recoveryPathView.push(<span key={'comma-' + idx}>{', '}</span>)
             } */
@@ -87,8 +89,10 @@ class HomeComponent extends React.Component {
 
         let worstHitView = []
         worstHit.forEach((s, idx) => {
-            if(idx === worstHit.length - 1) {
-                worstHitView.push(<span key="and">{' and '}</span>)
+            if(worstHit.length!==1){
+                if(idx === worstHit.length - 1) {
+                    worstHitView.push(<span key="and">{' and '}</span>)
+                }
             }/*  else {
                 recoveryPathView.push(<span key={'comma-' + idx}>{', '}</span>)
             } */
